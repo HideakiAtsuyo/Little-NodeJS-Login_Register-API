7bcdbbea7059ed2d44985f5aee6d083b956a3705
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-10-29 17:57:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,26 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').Server(app),
-    SIO = require('socket.io')(server),
-    chalk = require("chalk"),
-    Logger = require("./Utils/Logger.js"),
-    config = require("./Config.json");
-
-app.use(express.static(`${__dirname}/public`));
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'ejs');
-
-app.use('/', require('./routes/index'));
+const express = require("express"),
+	app = express(),
+	conf = require("./config.json"),
+	bodyParser = require("body-parser"),
+	Logger = require("./utils/logger.js"),
+	chalk = require("chalk");
 
 
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-const getDurationInMilliseconds = (start) => {
-    const NS_PER_SEC = 1e9
-    const NS_TO_MS = 1e6
-    const diff = process.hrtime(start)
 
-    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
-}
+app.use("/auth", require("./routes/auth"));
+app.use("/admin", require("./routes/admin"));
 
-app.use(function(req, res, next) {
-    const start = process.hrtime();
-    let durationInMilliseconds = 0;
-    res.status(404).render('error', { status: 404, error: "NOT FOUND", elapsedTime: getDurationInMilliseconds(start).toFixed(3) });
-    next();
-
+app.get('*', (req, res) => {
+  if(conf.getAnswer_deadmode == false){
+  	//Yeah no answer = no result for smart peoples(infinity charging then "RIP"..) :)
+  } else {
+  	res.send("Hello World :')");
+  }
 });
 
-SIO.on("connection", function(socket) {
+const PORT = conf.Port || 3001;
 
-});
-
-    
-server.listen(config.port, async () => {
-    console.clear();
-    console.log(chalk.blue(`
-        ╔═════════════════════════════════════════════╗
-        ║       web-chat By Hideaki | Port: ${chalk.green(config.port)}      ║
-        ╚═════════════════════════════════════════════╝
-    `));
-    /* DB Verification */
-
-    /* Others */
-    
-});
+app.listen(PORT, Logger.Info(`Successfully started on port: ${chalk.green(PORT)}`)); 
